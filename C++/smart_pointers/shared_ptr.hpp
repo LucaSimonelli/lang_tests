8d70c143daeb1e3e949cd1e4f9a902dbf48178f2
Luca Simonelli <lsimonelli@apple.com>
2016-02-25 10:20:41 +0000

    Renamed some files

--- C++/smart_pointers/shared_ptr.hpp
+++ C++/smart_pointers/shared_ptr.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-namespace owesome {
+namespace awesome {
 
-class Data {
+class ControlBlock {
   public:
-    Data(): mRefCount(0) {}
+    ControlBlock(): mRefCount(0) {}
     void incRefCount() {
       ++mRefCount;
     }
@@ -35,62 +35,62 @@ class shared_ptr {
     void deletePtr();
 
     T *mPtr;
-    Data *mData;
+    ControlBlock *mControlBlockPtr;
 };
 
 template<typename T>
-shared_ptr<T>::shared_ptr(): mPtr(nullptr), mData(nullptr) {}
+shared_ptr<T>::shared_ptr(): mPtr(nullptr), mControlBlockPtr(nullptr) {}
 
 template<typename T>
 shared_ptr<T>::shared_ptr(T *ptr): mPtr(ptr) {
-  mData = new Data;
-  mData->incRefCount();
+  mControlBlockPtr = new ControlBlock;
+  mControlBlockPtr->incRefCount();
 }
 
 template<typename T>
 shared_ptr<T>::~shared_ptr() {
-  if (mData->decRefCount() <= 0) {
+  if (mControlBlockPtr->decRefCount() <= 0) {
     deletePtr();
-    delete mData;
+    delete mControlBlockPtr;
   }
 }
 
 template<typename T>
 shared_ptr<T>::shared_ptr(const shared_ptr<T> & other) {
   if (this != &other) {
-    if (mData->decRefCount() <= 0) {
+    if (mControlBlockPtr->decRefCount() <= 0) {
       deletePtr();
-      delete mData;
+      delete mControlBlockPtr;
     }
     mPtr = other.mPtr;
-    mData = other.mData;
-    mData->incRefCount();
+    mControlBlockPtr = other.mControlBlockPtr;
+    mControlBlockPtr->incRefCount();
   }
 }
 
 template<typename T>
 shared_ptr<T>::shared_ptr(shared_ptr<T> && other) {
   if (this != &other) {
-    if (mData->decRefCount() <= 0) {
+    if (mControlBlockPtr->decRefCount() <= 0) {
       deletePtr();
-      delete mData;
+      delete mControlBlockPtr;
     }
     mPtr = other.mPtr;
-    mData = other.mData;
-    mData->incRefCount();
+    mControlBlockPtr = other.mControlBlockPtr;
+    mControlBlockPtr->incRefCount();
   }
 }
 
 template<typename T>
 shared_ptr<T> & shared_ptr<T>::operator=(const shared_ptr<T> & other) {
   if (this != &other) {
-    if (mData->decRefCount() <= 0) {
+    if (mControlBlockPtr->decRefCount() <= 0) {
       deletePtr();
-      delete mData;
+      delete mControlBlockPtr;
     }
     mPtr = other.mPtr;
-    mData = other.mData;
-    mData->incRefCount();
+    mControlBlockPtr = other.mControlBlockPtr;
+    mControlBlockPtr->incRefCount();
   }
   return *this;
 }
@@ -98,37 +98,37 @@ shared_ptr<T> & shared_ptr<T>::operator=(const shared_ptr<T> & other) {
 template<typename T>
 shared_ptr<T> & shared_ptr<T>::operator=(shared_ptr<T> && other) {
   if (this != &other) {
-    if (mData->decRefCount() <= 0) {
+    if (mControlBlockPtr->decRefCount() <= 0) {
       deletePtr();
-      delete mData;
+      delete mControlBlockPtr;
     }
     mPtr = other.mPtr;
-    mData = other.mData;
-    mData->incRefCount();
+    mControlBlockPtr = other.mControlBlockPtr;
+    mControlBlockPtr->incRefCount();
   }
   return *this;
 }
 
 template<typename T>
 void shared_ptr<T>::reset(T *ptr) {
-  if (mData->decRefCount() <= 0) {
+  if (mControlBlockPtr->decRefCount() <= 0) {
     deletePtr();
-    delete mData;
+    delete mControlBlockPtr;
   }
   mPtr = ptr;
-  mData = new Data;
-  mData->incRefCount();
+  mControlBlockPtr = new ControlBlock;
+  mControlBlockPtr->incRefCount();
 }
 
 template<typename T>
 T * shared_ptr<T>::release() {
   T * ptr = mPtr;
-  if (mData->decRefCount() <= 0) {
+  if (mControlBlockPtr->decRefCount() <= 0) {
     //deletePtr();
-    delete mData;
+    delete mControlBlockPtr;
   }
   mPtr = nullptr;
-  mData = nullptr;
+  mControlBlockPtr = nullptr;
   return ptr;
 }
 
---
+++ C++/smart_pointers/test_unique_ptr.cpp
@@ -0,0 +1,85 @@
+//#include <memory>
+#include "unique_ptr.hpp"
+#include <iostream>
+#include <vector>
+
+using std::unique_ptr;
+//using owesome::unique_ptr;
+
+class B {
+  public:
+    virtual int getInt() = 0;
+    int mInt;
+};
+
+class A {
+  public:
+  A(): a(10) { std::cout << "construct A" << std::endl; }
+  ~A() { std::cout << "destroy A" << std::endl; }
+  void foo() { std::cout << "I'm A" << std::endl; }
+  private:
+  int a;
+  std::vector<std::unique_ptr<B>> mVector;
+
+};
+
+static unique_ptr<A> gPtrA(new A);
+
+// unique_ptr for single object
+auto makeA() {
+  unique_ptr<A> ptrA(nullptr);
+  ptrA.reset(new A);
+  return ptrA;
+}
+
+/*
+// unique_ptr for single object
+auto makeAWithDelete() {
+  auto deleteA = [](A *ptrA) {
+    std::cout << "deleteA" << std::endl;
+    delete ptrA;
+  };
+  unique_ptr<A, decltype(deleteA)> ptrA(nullptr, deleteA);
+  ptrA.reset(new A);
+  return ptrA;
+}
+
+// unique_ptr for array, only for literature, never used
+// prefer std::vector of unique_ptr
+auto makeArrayAWithDelete(int size) {
+  auto deleteA = [](A *ptrA) {
+    std::cout << "deleteA" << std::endl;
+    delete[] ptrA;
+  };
+  unique_ptr<A[], decltype(deleteA)> ptrA(nullptr, deleteA);
+  ptrA.reset(new A[size]);
+  return ptrA;
+}
+*/
+int main(int argc, char * args[]) {
+  {
+    auto ptrA0 = makeA();
+  }
+  std::cout << "end of test" << std::endl;
+  /*
+  std::cout << "Allocate single A object" << std::endl;
+  auto ptrA = makeAWithDelete();
+  ptrA->foo();
+
+  std::cout << "Allocate array of A objects" << std::endl;
+  auto ptrArrayA = makeArrayAWithDelete(2);
+  ptrArrayA[0].foo();
+  std::cout << "Allocate single A object and assign it to a shared_ptr" << std::endl;
+  std::shared_ptr<A> ptrA2 = makeA();
+  ptrA2->foo();
+
+  */
+  std::cout << "Allocate std::vector of A objects" << std::endl;
+  std::vector< unique_ptr<A> > aVector;
+  //aVector[0] = makeA(); // Error! Segmentation fault!
+  aVector.push_back(makeA());
+  // error: call to implicitly-deleted copy constructor of unique_ptr
+  // you cannot copy a unique_ptr, unless you assign it to a std::shared_ptr
+  // auto ptrA2 = ptrA;
+  return 0;
+}
