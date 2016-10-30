9893de4d8c95f1d7bb561821747ca06287ae5e4a
Luca Simonelli <lsimonelli@apple.com>
2016-10-30 13:44:57 +0000

    More examples

--- C++/rtti/rtti.cpp
+++ C++/rtti/rtti.cpp
@@ -31,5 +31,9 @@ int main(int argc, char * argv[]) {
   A * ab1 = new B;
   B * bb1 = dynamic_cast<B*>(ab1);
   bb1->get();
+  B *pb = new B;
+  B &rb = *pb;
+  A &ra = rb;
+  B &rb2 = dynamic_cast<B&>(ra);
   return 0;
 }
